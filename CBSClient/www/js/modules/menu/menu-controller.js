08cd02c8eb2b596ed70b38b5f9d68fb17828473f
Filip Todoric <filiptodoric@gmail.com>
2016-04-08 04:36:35 -0400

    added simple menu

---
+++ CBSClient/www/js/modules/menu/menu-controller.js
@@ -0,0 +1,25 @@
+'use strict';
+
+function MenuController($scope, $state, $ionicModal, $timeout, $location) {
+
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+
+    $scope.bookAppt = function() {
+        $state.go('outside.register');
+    };
+
+    $scope.manage = function() {
+        $state.go('outside.manage');
+    };
+
+    $scope.donations = function() {
+        $state.go('outside.donations');
+    };
+
+    $scope.nurse = function() {
+        $state.go('outside.nurse');
+    };
+}
+module.exports = ['$scope', '$state', '$ionicModal', '$timeout','$location', MenuController];
