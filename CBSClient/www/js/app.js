009eaafa39028f9533e429cf47f84f7cadf60c7b
Guelor Emanuel <ncstech07@gmail.com>
2016-04-08 03:48:47 -0400

    Home page

--- CBSClient/www/js/app.js
+++ CBSClient/www/js/app.js
@@ -5,11 +5,13 @@ require('ionic');
 
 require('./modules/login/login');
 require('./modules/register/register');
+require('./modules/dashboard/dashboard');
 
 module.exports = angular.module('starter', [
     'ionic',
     'login',
-    'register'
+    'register',
+    'dashboard'
   ])
   .config(require('./router'))
   .run(require('./app-main'))
--- CBSClient/www/js/router.js
+++ CBSClient/www/js/router.js
@@ -20,6 +20,22 @@ module.exports = ['$stateProvider', '$urlRouterProvider',
             .state('outside.register', {
                 url: '/register',
                 templateUrl: 'js/modules/register/register.html'
+            })
+
+            .state('main', {
+              url: '/',
+              abstract: true,
+              templateUrl: 'js/modules/home/home.html'
+            })
+
+            .state('main.dash', {
+              url: 'main/dash',
+              views: {
+                'dash-tab': {
+                  templateUrl: 'js/modules/dashboard/dashboard.html',
+                  controller: 'DashBoardController as DashBoardController'
+                }
+              }
             });
         // if none of the above states are matched, use this as the fallback
         $urlRouterProvider.otherwise('/outside/login');
