c2de6ff21999327ab68485940a8ad9a62f3c2b2b
Filip Todoric <filiptodoric@gmail.com>
2016-04-08 00:24:23 -0400

    with nice ui and proper controller

--- CBSClient/www/js/modules/login/login-controller.js
+++ CBSClient/www/js/modules/login/login-controller.js
@@ -1,14 +1,14 @@
 'use strict';
 
-function LoginController($scope, $ionicModal, $timeout) {
-  
+function LoginController($scope, $ionicModal, $timeout, $location) {
+
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
   // listen for the $ionicView.enter event:
   //$scope.$on('$ionicView.enter', function(e) {
   //});
-  
+
   // Form data for the login modal
   $scope.loginData = {};
 
@@ -26,13 +26,15 @@ function LoginController($scope, $ionicModal, $timeout) {
 
   // Open the login modal
   $scope.login = function() {
+
     $scope.modal.show();
   };
 
   // Perform the login action when the user submits the login form
   $scope.doLogin = function() {
     console.log('Doing login', $scope.loginData);
-
+    $location.path('/outside/register');
+    console.log("console");
     // Simulate a login delay. Remove this and replace with your login
     // code if using a login system
     $timeout(function() {
@@ -40,4 +42,4 @@ function LoginController($scope, $ionicModal, $timeout) {
     }, 1000);
   };
 }
-module.exports = ['$scope', '$ionicModal', '$timeout', LoginController];
+module.exports = ['$scope', '$ionicModal', '$timeout','$location', LoginController];
