e69b6e48eee14f438e791297d5a1f509b33f91b3
Guelor Emanuel <ncstech07@gmail.com>
2016-04-08 04:07:07 -0400

    fixed

--- CBSClient/www/js/router.js
+++ CBSClient/www/js/router.js
@@ -39,6 +39,22 @@ module.exports = ['$stateProvider', '$urlRouterProvider',
                 url: '/register',
                 templateUrl: 'js/modules/register/register.html',
                 controller: 'RegisterController as registerController'
+            })
+
+            .state('main', {
+              url: '/',
+              abstract: true,
+              templateUrl: 'js/modules/home/home.html'
+            })
+
+            .state('main.dash', {
+              url: 'main/dash',
+              views: {
+                'dash-tab': {
+                  templateUrl: 'js/modules/dashboard/dashboard.html',
+                  controller: 'DashBoardController as DashBoardController'
+                }
+              }
             });
         // if none of the above states are matched, use this as the fallback
         $urlRouterProvider.otherwise('/outside/login');
